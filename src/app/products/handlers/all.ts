e48edb0800550943957b2267ca47cd43a0b1ad5c
Denis Sakharov <denis.sakharov@icloud.com>
2025-03-02 17:59:24 +0400

    Remove getAllProducts.query.ts

--- src/app/products/handlers/all.ts
+++ src/app/products/handlers/all.ts
@@ -1,9 +1,25 @@
 import DBDataSource from '../../../db/data-source';
 import { RouteGenericInterface, RouteHandler } from 'fastify';
-import getAllProductsQuery from '../../../db/queries/getAllProducts.query';
+import StorageEvent from '../../../db/entities/storage-event';
+
+interface ProductInfo {
+  productId: string;
+  productName: string | null;
+  shelfLifeDays: string | null;
+  createdAt: Date;
+}
 
 const handler: RouteHandler<RouteGenericInterface> = async () => {
-  return await DBDataSource.manager.query(getAllProductsQuery());
+  const storageEventsRepository = DBDataSource.getRepository(StorageEvent);
+
+  return await storageEventsRepository
+    .createQueryBuilder('se')
+    .select('se.productId', 'productId')
+    .addSelect("MAX(se.data ->> 'product_name')", 'productName')
+    .addSelect("MAX(se.data ->> 'shelf_life_days')", 'shelfLifeDays')
+    .addSelect('MIN(se.createdAt)', 'createdAt')
+    .groupBy('se.productId')
+    .getRawMany<ProductInfo>();
 };
 
 export default handler;
